debd3a65a85132e76d93c7ff975c76d19601b5ec
Ghower <1520567597@qq.com>
2021-12-13 13:54:46 +0800

    doc: 初始化

---
+++ src/main/java/com/ghower/webtemplate/WebbootTemplateApplication.java
@@ -0,0 +1,13 @@
+package com.ghower.webtemplate;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class WebbootTemplateApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(WebbootTemplateApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/ghower/webtemplate/WebbootTemplateApplicationTests.java
@@ -0,0 +1,13 @@
+package com.ghower.webtemplate;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class WebbootTemplateApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
